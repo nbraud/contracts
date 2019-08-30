b642352887fc22eca522b9f3497514d3ebf3fa60
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-08-31 00:42:57 +0200

    setup.py: Remove unecessary version slice

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 import sys
 
-if sys.version_info[:2] < (3, 5):
+if sys.version_info < (3, 5):
     sys.stderr.write(
         'This version of dpcontracts requires Python 3.5 - either upgrade '
         'to a newer version of pip that handles this automatically, or '
